f402c2741fe90021d449ea3929f7448e31efcc61
Kovacsics Robert (NixOS) <kovirobi@gmail.com>
2016-02-06 21:09:12 +0000

    WP4: Bug fix: improper <init> for InnerClasses, introduced by dc34e6dd2183a7db5c774f1dcbee4386f9bb1c99

--- src/main/java/rmk35/partIIProject/backend/InnerClass.java
+++ src/main/java/rmk35/partIIProject/backend/InnerClass.java
@@ -39,18 +39,23 @@ public class InnerClass extends OutputClass
     this.closureVariables = closureVariables;
     this.variableCount = variableCount;
 
+    constructorTypes = new ObjectType[closureVariables.size()];
+    Arrays.fill(constructorTypes, runtimeValueType);
     // Ensure closure variables exist and also that they are set on construction
-    ByteCodeMethod initializerMethod = this.getInitializer();
+    ByteCodeMethod initializerMethod = new ByteCodeMethod(voidType, "public", "<init>", constructorTypes);; // FIXME: arguments to initializer!
+    initializerMethod.addInstruction(new LocalLoadInstruction(new ObjectType(), 0));
+    initializerMethod.addInstruction(new NonVirtualCallInstruction(voidType, getSuperClassName() + "/<init>"));
     int index = 1; // 0 is 'this'
     for (IdentifierStatement identifier : closureVariables)
     { ensureFieldExists("private", identifier.getName(), runtimeValueType);
+      initializerMethod.ensureLocal(index);
       initializerMethod.addInstruction(new LocalLoadInstruction(runtimeValueType, 0)); // This
       initializerMethod.addInstruction(new LocalLoadInstruction(runtimeValueType, index)); // Value
       initializerMethod.addInstruction(new PutFieldInstruction(runtimeValueType, getName() + "/" + identifier.getName()));
       index++;
     }
-    constructorTypes = new ObjectType[closureVariables.size()];
-    Arrays.fill(constructorTypes, runtimeValueType);
+    // Overwrite initializer
+    methods.put("<init>", initializerMethod);
 
     ByteCodeMethod runMethod = new ByteCodeMethod(runtimeValueType, "public", "run", listType);
     // Store array into locals
