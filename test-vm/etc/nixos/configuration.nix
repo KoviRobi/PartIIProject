67d866dcf2e77d3da07b30e69e68cb868478625d
Kovacsics Robert (NixOS) <kovirobi@gmail.com>
2016-05-04 15:39:06 +0100

    MISC: EVAL: Updated VM config

--- test-vm/etc/nixos/configuration.nix
+++ test-vm/etc/nixos/configuration.nix
@@ -8,28 +8,13 @@
 # Note, using VM clock, not real clock
 # log-in with name: "guest" and password: "vm"
 # SHARED_DIR=/home/kr2/Dokumentumok/PartII-Project/part-II-project/
-# export QEMU_KERNEL_PARAMS SHARED_DIR
+# export SHARED_DIR
 # ./result/bin/run-vmhost-vm -rtc 'base=utc,clock=vm'
 
 # Then /tmp/shared is mounted to wherever you have set $SHARED_DIR
 
-# Can build the project with (if you are in project-root and have the file jasmin-2.4/jasmin.jar)
-# awk 'BEGIN{RS="```"; FS="\n"; OFS="\n"} /^bash/{$1=""; print $0}' < BUILD.md | sh
-# which just reads out the commands to execute from BUILD.md
-
-# Also remeber to put results into a tmpfs mount, I have used
-# mkdir /tmp/ram
-# sudo mount -t tmpfs tmpfs /tmp/ram
-# then move them onto the shared store when they are done
-# and prefix time-dependent tests with `sudo nice -n -20` so that they
-# are interrupted less often
-
 { config, pkgs, ... }:
-{ environment.systemPackages = with pkgs; [
-    wget
-    jdk
-    maven
-  ];
+{ environment.systemPackages = with pkgs; [ jdk maven ];
 
   fileSystems."/".label = "vmdisk";
 
